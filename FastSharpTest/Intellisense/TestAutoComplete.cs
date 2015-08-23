4630917973a5c8bf10a265764548e3fcb0668e9f
Nayan Shah <Nayan.Shah@microsoft.com>
2015-08-22 20:24:22 -0700

    Adding a unit test project

---
+++ FastSharpTest/Intellisense/TestAutoComplete.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FastSharpTest.Intellisense
+{
+    [TestClass]
+    public class TestAutoComplete
+    {
+        [TestMethod]
+        public void TestRegisterAssembly()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
